86967eade72f35914c6e786d2cf034a4bcb4a0a8
mybene <benoite251@gmial.com>
2019-10-04 13:43:19 +0200

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/media/bin/media
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,23 +3,28 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class DB {
         private static URI dbUri;
         public static Sql2o sql2o;
-        static Logger logger = LoggerFactory.getLogger(DB.class);
+
         static {
+                Logger logger = LoggerFactory.getLogger(DB.class);
 
                 try {
-                        if (System.getenv("postgresql-dimensional-10461") == null) {
+                        if (System.getenv("DATABASE_URL") == null) {
                                 dbUri = new URI("postgres://localhost:5432/media");
+                                logger.info("Using local database.");
                         } else {
-                                dbUri = new URI(System.getenv("postgresql-dimensional-10461"));
+                                dbUri = new URI(System.getenv("DATABASE_URL"));
                         }
+
                         int port = dbUri.getPort();
                         String host = dbUri.getHost();
                         String path = dbUri.getPath();
-                        String username = (dbUri.getUserInfo() == null) ? umimogzlysxckw: dbUri.getUserInfo().split(":")[0];
-                        String password = (dbUri.getUserInfo() == null) ? 08c28bfbea02d1e27d2db1844a9dfadbb3b6b53e738c59b270af9b3741b1bf7 : dbUri.getUserInfo().split(":")[1];
+                        String username = (dbUri.getUserInfo() == null) ? "wecode" : dbUri.getUserInfo().split(":")[0];
+                        String password = (dbUri.getUserInfo() == null) ? "12307" : dbUri.getUserInfo().split(":")[1];
+
                         sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
                 } catch (URISyntaxException e ) {
                         logger.error("Unable to connect to database.");
